8e5df7faca88866f114f58a6661e7212252ecdc3
Pamela Frieze <pfrieze@gmail.com>
2019-02-11 21:54:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function BoardMember(name, homeState, training){
    BoardMember.prototype.veto = function(){
      return 'No, I must disagree'
    }
+   
    BoardMember.prototype.approve = function(){
      return 'You can do that!'
    }
