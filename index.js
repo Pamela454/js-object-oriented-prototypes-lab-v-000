43757d519d3bebe957891fdfb9efab056742d614
Pamela Frieze <pfrieze@gmail.com>
2019-02-11 21:56:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ function BoardMember(name, homeState, training){
   BoardMember.prototype.releasePressStatement = function(){
     return "You will see great things from Scuber."
   }
-//
-//   this.sayHi = function(){
-//     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-//   }
+
+ BoardMember.prototype.sayHi = function(){
+     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+   }
  }
