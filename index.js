57110abcf04ec1c438a331fa01a424cb8ab399fa
Pamela Frieze <pfrieze@gmail.com>
2019-02-11 21:55:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,10 +10,11 @@ function BoardMember(name, homeState, training){
    BoardMember.prototype.approve = function(){
      return 'You can do that!'
    }
-//   this.doCharity = function(){
-//     return "I like to help people."
-//   }
-//
+  
+  BoardMember.prototype.doCharity = function(){
+    return "I like to help people."
+  }
+
 //   this.releasePressStatement = function(){
 //     return "You will see great things from Scuber."
 //   }
